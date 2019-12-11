9f9a334a2910db020908c74ff4ec1af6980d7be4
Ben Borla <ben.borla@lamudi.com>
2019-12-11 15:26:39 +0800

    updates

--- themes/ZetaMedsTheme/assets/js/sylius-add-to-cart.js
+++ themes/ZetaMedsTheme/assets/js/sylius-add-to-cart.js
@@ -19,7 +19,18 @@ const SyliusAddToCart = (el) => {
   const validationElement = element.querySelector('[data-js-add-to-cart="error"]');
 
   element.addEventListener('submit', (e) => {
-    const request = axios.post(url, serialize(element));
+    const payload = JSON.parse(element.dataset.payload);
+    let elements = serialize(element, { hash: true});
+
+    // merge payload to elements
+    const newPayload = {
+      sylius_add_to_cart: {
+        cartItem: payload,
+        _token: elements.sylius_add_to_cart._token
+      }
+    };
+
+    const request = axios.post(url, encodeURI(getUrlString(newPayload)));
 
     e.preventDefault();
 
@@ -42,4 +53,66 @@ const SyliusAddToCart = (el) => {
   });
 };
 
+const serializeToJSON = str =>
+  str.split('&')
+    .map(x => x.split('='))
+    .reduce((acc, [key, value]) => ({
+      ...acc,
+      [key]: isNaN(value) ? value : Number(value)
+    }),
+{});
+
+const getUrlString = (params, keys = [], isArray = false) => {
+  const p = Object.keys(params).map(key => {
+    let val = params[key]
+
+    if ("[object Object]" === Object.prototype.toString.call(val) || Array.isArray(val)) {
+      if (Array.isArray(params)) {
+        keys.push("");
+      } else {
+        keys.push(key)
+      }
+      return getUrlString(val, keys, Array.isArray(val))
+    } else {
+      let tKey = key
+
+      if (keys.length > 0) {
+        const tKeys = isArray ? keys : [...keys, key]
+        tKey = tKeys.reduce((str, k) => { return "" === str ? k : `${str}[${k}]` }, "")
+      }
+      if (isArray) {
+        return `${ tKey }[]=${ val }`
+      } else {
+        return `${ tKey }=${ val }`
+      }
+
+    }
+  }).join('&')
+
+  keys.pop()
+  return p
+};
+
+// add data-status on submit button to know which one is triggered
+const buttons = document.querySelectorAll('form[name=sylius_add_to_cart] button');
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const target = button.dataset.target;
+    const variant = button.dataset.variant || '';
+    const domQty= document.querySelector(`#${target}`);
+    const quantity = domQty.value !== null ? domQty.value : button.dataset.quantity;
+    const form = document.querySelector('form[name=sylius_add_to_cart]');
+    let payload = { quantity };
+
+    if (variant.length) {
+      payload = { ...payload, ...{ variant } };
+    }
+
+    form.setAttribute('data-payload', JSON.stringify(payload));
+    document.querySelector('button#add_to_cart_final').click();
+  });
+});
+
 export default SyliusAddToCart;
+
+
